3540892040659afe84b29c29eef2e42071230473
Spyros Psarras <spyridon@adstate.com>
2021-07-30 01:53:51 +0200

    Fix qli file download location. Add deps on gitignore.

--- azure-pipelines/download_install_qt.ps1
+++ azure-pipelines/download_install_qt.ps1
@@ -2,12 +2,13 @@ $ErrorActionPreference = "Stop"
 
 $qli_install_version = '2019.05.26.1'
 $qt_version = '5.13.0'
+$qli_path = $(Split-Path -Parent $PSCommandPath) + '\deps\qli-installer.zip'
 
 New-Item -Force -ItemType Directory -Path ".\deps\"
 
 Write-Output 'Downloading QLI Installer'
 $Wc = New-Object System.Net.WebClient
-$Wc.DownloadFile("https://github.com/nelsonjchen/qli-installer/archive/v$qli_install_version.zip", '.\deps\qli-installer.zip') ;
+$Wc.DownloadFile("https://github.com/nelsonjchen/qli-installer/archive/v$qli_install_version.zip", $qli_path) ;
 Write-Output 'Downloaded QLI Installer'
 
 Write-Output 'Extracting QLI Installer'

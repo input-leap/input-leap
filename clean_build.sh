2a8fb4338fca2166cc7722bb89cfa59de92dfe2b
Dom Rodriguez <shymega@shymega.org.uk>
2021-11-28 22:33:47 +0000

    Rename osx_environment -> macos_environment
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- clean_build.sh
+++ clean_build.sh
@@ -11,9 +11,8 @@ fi
 # default build configuration
 B_BUILD_TYPE=${B_BUILD_TYPE:-Debug}
 if [ "$(uname)" = "Darwin" ]; then
-    # OSX needs a lot of extra help, poor thing
-    # run the osx_environment.sh script to fix paths
-    . ./osx_environment.sh
+    # macOS needs a little help, so we source this environment script to fix paths.
+    . ./macos_environment.sh
     B_CMAKE_FLAGS="-DCMAKE_OSX_SYSROOT=$(xcode-select --print-path)/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk -DCMAKE_OSX_DEPLOYMENT_TARGET=10.9 $B_CMAKE_FLAGS"
 fi
 # allow local customizations to build environment

0c3cf10612697a734a8851b7668ba3da12f5fedc
Peter Hutterer <peter.hutterer@who-t.net>
2022-08-08 15:56:54 +1000

    Improve the clean_build.sh script
    - Simplify the check for cmake3/cmake
    - Drop the (broken) version check and instead set
      CMAKE_BUILD_PARALLEL_LEVEL
    - Default to nproc jobs for the parallel build
    - use set -e instead of appending `|| exit 1` everywhere
    - drop the custom backend invocation and rely on cmake build instead
    - make the build directory configurable (via env var)

--- clean_build.sh
+++ clean_build.sh
@@ -4,26 +4,28 @@ cd "$(dirname "$0")" || exit 1
 
 # some environments have cmake v2 as 'cmake' and v3 as 'cmake3'
 # check for cmake3 first then fallback to just cmake
-
-B_CMAKE=$(command -v cmake3 2>/dev/null)
-if [ "$?" -eq 0 ]; then
-    continue
-else
-    if command -v cmake 2>/dev/null; then
-        B_CMAKE=$(command -v cmake)
-    else
-        echo "ERROR: CMake not in $PATH, cannot build! Please install CMake, or if this persists, file a bug report."
-        exit 1
-    fi
+[ -n "$B_CMAKE" ] || B_CMAKE=$(command -v cmake3)
+[ -n "$B_CMAKE" ] || B_CMAKE=$(command -v cmake)
+if [ -z "$B_CMAKE" ]; then
+    echo "ERROR: CMake not in $PATH, cannot build! Please install CMake, or if this persists, file a bug report."
+    exit 1
 fi
 
-CMAKE_VERSION="$("$B_CMAKE" --version)"
-if [ "$(printf '%s\n' "3.12.0" "$currentver" | sort -V | head -n1)" = "3.12.0" ]; then
-    CMAKE_HAS_PARALLEL=1
+if [ "$(uname)" = "Darwin" ]; then
+    nproc=$(sysctl -n hw.ncpu)
 else
-    CMAKE_HAS_PARALLEL=0
+    nproc=$(nproc)
+fi
+if [ -z "$nproc" ]; then
+    nproc=8
+fi
+if [ $nproc -gt 32 ]; then
+    nproc=32
 fi
 
+export CMAKE_BUILD_PARALLEL_LEVEL=${nproc}
+
+B_BUILD_DIR="${B_BUILD_DIR:-build}"
 B_BUILD_TYPE="${B_BUILD_TYPE:-Debug}"
 B_CMAKE_FLAGS="-DCMAKE_BUILD_TYPE=${B_BUILD_TYPE} ${B_CMAKE_FLAGS:-}"
 
@@ -41,23 +43,15 @@ fi
 # allow local customizations to build environment
 [ -r ./build_env.sh ] && . ./build_env.sh
 
+set -e
+
 # Initialise Git submodules
 git submodule update --init --recursive
 
-rm -rf build
-mkdir build || exit 1
-cd build || exit 1
-echo "Starting Input Leap $B_BUILD_TYPE build..."
-"$B_CMAKE" $B_CMAKE_FLAGS .. || exit 1
-
-if [ "$CMAKE_HAS_PARALLEL" -eq "1" ]; then
-    "$B_CMAKE" --build . --parallel || exit 1
-else
-    if command -v ninja 2>/dev/null; then
-        ninja || exit 1
-    else
-        make || exit 1
-    fi
-fi
-
+rm -rf ${B_BUILD_DIR}
+mkdir ${B_BUILD_DIR}
+cd ${B_BUILD_DIR}
+echo "Starting Input Leap $B_BUILD_TYPE build in '${B_BUILD_DIR}'..."
+"$B_CMAKE" $B_CMAKE_FLAGS ..
+"$B_CMAKE" --build .
 echo "Build completed successfully"

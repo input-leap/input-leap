68667d17eb8f959ed03be139403bcc88834e4ff0
draekko <draekko.software@gmail.com>
2023-02-23 06:05:52 -0500

    updated headers for new x11 converters for input-leaf

--- src/lib/platform/XWindowsClipboardJPGConverter.h
+++ src/lib/platform/XWindowsClipboardJPGConverter.h
@@ -1,5 +1,6 @@
 /*
- * barrier -- mouse and keyboard sharing utility
+ * InputLeap -- mouse and keyboard sharing utility
+ * Copyright (C) 2023 Draekko
  * Copyright (C) 2012-2016 Symless Ltd.
  * Copyright (C) 2004 Chris Schoeneman
  *

8a9d052e14a2857fcf1837296d6c0bb258d01058
Wassim Vitols <wassim.vitols@gmail.com>
2024-01-04 12:01:12 +1100

    Handle super modifier in EiKeyState

--- src/lib/platform/EiKeyState.cpp
+++ src/lib/platform/EiKeyState.cpp
@@ -141,6 +141,8 @@ std::uint32_t EiKeyState::convert_mod_mask(std::uint32_t xkb_mask) const
             barrier_mask |= (1 << kKeyModifierBitControl);
         else if (strcmp(XKB_MOD_NAME_ALT, name) == 0)
             barrier_mask |= (1 << kKeyModifierBitAlt);
+        else if (strcmp(XKB_MOD_NAME_LOGO, name) == 0)
+            barrier_mask |= (1 << kKeyModifierBitSuper);
     }
 
     return barrier_mask;

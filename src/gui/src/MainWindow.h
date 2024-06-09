5fff6dc65c2bf390c37280326fff86005ff2677f
sithlord48 <sithlord48@gmail.com>
2024-06-09 13:53:50 -0400

    fix: broken autoconnect

--- src/gui/src/MainWindow.h
+++ src/gui/src/MainWindow.h
@@ -198,7 +198,7 @@ public slots:
 
 private slots:
     void on_m_pCheckBoxAutoConfig_toggled(bool checked);
-    void on_m_pComboServerList_currentIndexChanged(QString );
+    void comboServerList_currentIndexChanged(QString );
     void on_m_pButtonReload_clicked();
     void installBonjour();
 

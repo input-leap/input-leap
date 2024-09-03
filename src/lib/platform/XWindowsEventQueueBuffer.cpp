d674048097966bd3a92569d1666655ed18767d4e
Peter Hutterer <peter.hutterer@who-t.net>
2024-09-03 13:17:58 +1000

    platform: use pipe2() instead of separate calls to fcntl

--- src/lib/platform/XWindowsEventQueueBuffer.cpp
+++ src/lib/platform/XWindowsEventQueueBuffer.cpp
@@ -41,14 +41,8 @@ XWindowsEventQueueBuffer::XWindowsEventQueueBuffer(IXWindowsImpl* impl,
 
     m_userEvent = m_impl->XInternAtom(m_display, "INPUTLEAP_USER_EVENT", False);
     // set up for pipe hack
-    int result = pipe(m_pipefd);
+    int result = pipe2(m_pipefd, O_NONBLOCK);
     assert(result == 0);
-
-    int pipeflags;
-    pipeflags = fcntl(m_pipefd[0], F_GETFL);
-    fcntl(m_pipefd[0], F_SETFL, pipeflags | O_NONBLOCK);
-    pipeflags = fcntl(m_pipefd[1], F_GETFL);
-    fcntl(m_pipefd[1], F_SETFL, pipeflags | O_NONBLOCK);
 }
 
 XWindowsEventQueueBuffer::~XWindowsEventQueueBuffer()

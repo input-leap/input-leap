input-leap/input-leap

commit 18eb4518aad5697f4f1bb860c0c4d7ab0fc2d281

Use ~/.config to store user settings on linux, update config path in server documentation
Fixes #1851  by correcting the path in the manual

src/lib/inputleap/ServerApp.cpp
@@ -118,9 +118,9 @@ ServerApp::help()
         profile_path = inputleap::DataDirectories::profile();
     }
 
-    auto usr_config_path = (profile_path / inputleap::fs::u8path(USR_CONFIG_NAME)).u8string();
+    auto usr_config_path = (profile_path / inputleap::fs::u8path(CONFIG_NAME)).u8string();
     auto sys_config_path = (inputleap::DataDirectories::systemconfig() /
-                            inputleap::fs::u8path(SYS_CONFIG_NAME)).u8string();
+                            inputleap::fs::u8path(CONFIG_NAME)).u8string();
 
     std::ostringstream buffer;
     buffer << "Start the InputLeap server component. The server shares the keyboard &\n"
@@ -208,7 +208,7 @@ ServerApp::loadConfig()
         auto path = inputleap::DataDirectories::profile();
         if (!path.empty()) {
             // complete path
-            path /= inputleap::fs::u8path(USR_CONFIG_NAME);
+            path /= inputleap::fs::u8path(CONFIG_NAME);
 
             // now try loading the user's configuration
             if (loadConfig(path.u8string())) {
@@ -220,7 +220,7 @@ ServerApp::loadConfig()
             // try the system-wide config file
             path = inputleap::DataDirectories::systemconfig();
             if (!path.empty()) {
-                path /= inputleap::fs::u8path(SYS_CONFIG_NAME);
+                path /= inputleap::fs::u8path(CONFIG_NAME);
                 if (loadConfig(path.u8string())) {
                     loaded            = true;
                     args().m_configFile = path.u8string();

doc/input-leaps.1
@@ -71,7 +71,7 @@ interfaces using port number 24800.
 If no configuration file pathname is provided then the first of the
 following to load successfully sets the configuration:
 .IP
-.I $HOME/.local/share/input-leap/.input-leap.conf
+.I $HOME/.config/InputLeap/input-leap.conf
 .br
 .I /etc/input-leap.conf
 .SH COPYRIGHT

src/lib/common/unix/DataDirectories.cpp
@@ -65,10 +65,10 @@ static fs::path profile_basedir()
 #if defined(WINAPI_XWINDOWS) || defined(WINAPI_LIBEI)
     // linux/bsd adheres to freedesktop standards
     // https://standards.freedesktop.org/basedir-spec/basedir-spec-latest.html
-    const char* dir = std::getenv("XDG_DATA_HOME");
+    const char* dir = std::getenv("XDG_CONFIG_HOME");
     if (dir != nullptr)
         return fs::u8path(dir);
-    return unix_home() / ".local/share";
+    return unix_home() / ".config/";
 #else
     // macos has its own standards
     // https://developer.apple.com/library/content/documentation/General/Conceptual/MOSXAppProgrammingGuide/AppRuntime/AppRuntime.html

src/lib/inputleap/ServerApp.h
@@ -118,11 +118,9 @@ private:
 
 // configuration file name
 #if SYSAPI_WIN32
-#define USR_CONFIG_NAME "input-leap.sgc"
-#define SYS_CONFIG_NAME "input-leap.sgc"
+#define CONFIG_NAME "input-leap.sgc"
 #elif SYSAPI_UNIX
-#define USR_CONFIG_NAME ".input-leap.conf"
-#define SYS_CONFIG_NAME "input-leap.conf"
+#define CONFIG_NAME "input-leap.conf"
 #endif
 
 } // namespace inputleap

commit 8e1c074326ce1c95e96ef9b58bb9baa12eaca60f

lib: Load server config from old, deprecated path
A warning is printed if old config path is found, but not the new one.

Fixes: 18eb4518aad5697f4f1bb860c0c4d7ab0fc2d281

src/lib/inputleap/ServerApp.cpp
@@ -205,25 +205,45 @@ ServerApp::loadConfig()
 
     // load the default configuration if no explicit file given
     else {
-        auto path = inputleap::DataDirectories::profile();
-        if (!path.empty()) {
-            // complete path
-            path /= inputleap::fs::u8path(CONFIG_NAME);
+        struct PathConfig
+        {
+            fs::path root;
+            std::string filename;
+            bool deprecated = false;
+        };
 
-            // now try loading the user's configuration
-            if (loadConfig(path.u8string())) {
-                loaded            = true;
-                args().m_configFile = path.u8string();
+        std::vector<PathConfig> path_configs = {
+            {
+                inputleap::DataDirectories::profile(),
+                CONFIG_NAME,
+                false
+            },
+            {
+                inputleap::DataDirectories::profile(),
+                ".input-leap.conf", // used before 3.0.0
+                true
+            },
+            {
+                inputleap::DataDirectories::systemconfig(),
+                CONFIG_NAME,
+                false
             }
-        }
-        if (!loaded) {
-            // try the system-wide config file
-            path = inputleap::DataDirectories::systemconfig();
+        };
+
+        for (const auto& path_config : path_configs) {
+            auto path = path_config.root;
             if (!path.empty()) {
-                path /= inputleap::fs::u8path(CONFIG_NAME);
+                path /= inputleap::fs::u8path(path_config.filename);
                 if (loadConfig(path.u8string())) {
-                    loaded            = true;
+                    if (path_config.deprecated) {
+                        LOG_PRINT("%s: Loading config from deprecated path %s, please use %s",
+                                  args().m_exename.c_str(),
+                                  path.u8string().c_str(),
+                                  (path_configs[0].root / path_configs[0].filename).u8string().c_str());
+                    }
+                    loaded = true;
                     args().m_configFile = path.u8string();
+                    break;
                 }
             }
         }